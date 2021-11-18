71eaacb77d3308744464424270be07b02ffe7f31
K.M Shariat Ullah <cast.shariat@gmail.com>
2021-11-18 14:54:20 +0600

    Create diceroll.py

---
+++ diceroll.py
@@ -0,0 +1,7 @@
+#rolling-a-dice
+#Using random integer function in random module is the easiest way to make rolling dice function
+
+import random
+def diceRool(num):
+  return random.randint(1,num)
+diceRoll(6)
