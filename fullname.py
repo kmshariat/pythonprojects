a1bb85ed1ec161c4c4022a1ff032502cfe3ebc9c
K.M Shariat Ullah <cast.shariat@gmail.com>
2022-01-12 18:49:04 +0600

    Create fullname.py

---
+++ fullname.py
@@ -0,0 +1,5 @@
+#this code will take first name and last name as input and print the full name as output
+#WARNING: This code (i.e. format method) doesn't work in Python 2
+first_name = input("Enter your first name ")
+last_name = input("Enter your last name ")
+print(f"Hello, Mr. {last_name}! Your full name is {first_name} {last_name}")
