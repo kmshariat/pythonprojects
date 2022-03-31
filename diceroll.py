afd5b1e73d9e268c58c827fa037976fc3f6b6530
K.M Shariat Ullah <cast.shariat@gmail.com>
2022-03-31 23:27:27 +0600

    fixed typo and range

--- diceroll.py
+++ diceroll.py
@@ -1,7 +1,6 @@
-#rolling-a-dice
-#Using random integer function in random module is the easiest way to make rolling dice function
+# rolling-a-dice which has six sides
 
 import random
-def diceRool(num):
-  return random.randint(1,num)
-diceRoll(6)
+def diceRoll():
+  return random.randint(1,6)
+diceRoll()
