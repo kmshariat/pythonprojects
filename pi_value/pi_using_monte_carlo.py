50dd5015dbfdaaa4ea80aba8d727a84dbc989d62
K.M Shariat Ullah <cast.shariat@gmail.com>
2022-06-25 21:56:23 +0600

    fixed bug

--- pi_value/pi_using_monte_carlo.py
+++ pi_value/pi_using_monte_carlo.py
@@ -1,11 +1,15 @@
-#unfinished
 import random
-x = random.uniform(0,1)
-y = random.uniform(0,1)
-pt_in = 0.0
-pt_t = 0.0
-for i in range(1000):
-    if x**2+y**2 < 1:
-        pt_in +=1
-    pt_t +=1
-print(pt_in/pt_t)
+
+pt_in = 0 #points inside the quarter circle
+pt_t = 0 #points inside the whole quarter square
+
+n = int(input("How many iterations do we want? "))
+
+for i in range(n):
+  x = random.uniform(0,1) #random.uniform(a,b) takes a random uniformly distributed number between a and b
+  y = random.uniform(0,1)
+  if x**2+y**2 <= 1: #x**2 + y**2 <=1 justifies if the point is inside the circle or not
+    pt_in +=1 
+  pt_t +=1
+  
+print(4*(pt_in/pt_t)) 
