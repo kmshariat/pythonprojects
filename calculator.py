782322a47a7ba652493cfdcf94b68564f8559e92
K.M Shariat Ullah <cast.shariat@gmail.com>
2022-06-26 11:22:25 +0600

    added directions

--- calculator.py
+++ calculator.py
@@ -1,5 +1,9 @@
 #Better Calculator
 num1 = float(input("Enter first number: "))
+print("----------")
+print("Directions")
+print("----------")
+print("Enter Operator + to add, - to subtract, * to multiply and / to divide")
 op = input("Enter Operator: ")
 num2 = float(input("Enter another number: "))
 if op == "+":
