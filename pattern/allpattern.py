3a8f2f4d9b2816b42d680486e42d7b07a48e7deb
K.M Shariat Ullah <cast.shariat@gmail.com>
2022-07-01 16:53:46 +0600

    added function
    added pattern_same and pattern_diff and made pyramid a function

---
+++ pattern/allpattern.py
@@ -0,0 +1,26 @@
+#this function will take two inputs the number that I want to print and the row number about how many rows I want to print. 
+def pattern_same(num,row):
+  for i in range(row):
+    print(str(num)*(i+1))
+#pattern_same(2,3) will return
+#2
+#22
+#222
+
+#this function will take the number of rows. It will print number i in the i th row
+def pattern_diff(row):
+  for i in range(row):
+    print(str((i+1))*(i+1))
+#pattern_diff(3) will return 
+#1
+#22
+#333 
+
+#this function will take the number of rows. It will print * symbol upto row number.
+def pyramid(row):
+  for i in range(row+1):
+    print(" "*(row-i)+" *"*i) #the spaces will create a uniformly distributed pyramid
+#pyramid(3) will return
+# *
+# **
+# ***
--- pattern/pyramidStar.py
+++
@@ -1,4 +0,0 @@
-#pyramid
-row = int(input ("Enter how many rows: "))
-for i in range(row+1):
-  print(" "*(row-i)+" *"*i)
