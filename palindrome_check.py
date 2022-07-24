970eeca49a973c02f0a79c0526f3244de8c2b632
K.M Shariat Ullah <cast.shariat@gmail.com>
2022-07-24 19:32:27 +0600

    updated datatype of the function parameter

--- palindrome_check.py
+++ palindrome_check.py
@@ -1,7 +1,11 @@
-word = input("Enter a word ")
-word = word.lower()
-reverse_word = word[::-1]
-if word == reverse_word:
-  print(f"{word} is a palindrome")
-else:
-  print(f"{word} is not a palindrome")
+def palindrome(word):
+    word = str(word).lower()
+    reverse_word = word[::-1]
+    if word == reverse_word:
+      print(f"{word} is a palindrome")
+    else:
+      print(f"{word} is not a palindrome")
+      
+#test_case 1: palindrome('MOM') -> mom is a palindrome
+#test_case 2: palindrome('wizard') -> wizard is not a palindrome
+#test_case 3: palindrome(3829) -> 3829 is not a palindrome
