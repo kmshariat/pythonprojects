b89eb340ea80013b97585940dcb5b095ed586493
K.M Shariat Ullah <cast.shariat@gmail.com>
2022-09-29 18:39:47 +0600

    Create star.py

---
+++ turtle/star.py
@@ -0,0 +1,9 @@
+import turtle
+
+#this function creates a regular sized star with 5 sides
+def star():
+    for i in range(5):
+        turtle.forward(100)
+        turtle.right(144)
+    turtle.done()
+star()
